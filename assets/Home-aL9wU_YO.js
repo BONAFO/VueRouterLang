458d1c3b01fc5f9d92e0a49c8cadc20a841eaff8
BONAFO <91232074+BONAFO@users.noreply.github.com>
2024-05-22 09:26:51 -0300

    Add files via upload

---
+++ assets/Home-aL9wU_YO.js
@@ -0,0 +1,1 @@
+import{u as n}from"./index-BPh78xor.js";function r({lang:t}){return{title:"GAME 01"}}const s={__name:"Home",setup(t){const e=n().params;return r({lang:e.lang}),(a,u)=>null}};export{s as default};
