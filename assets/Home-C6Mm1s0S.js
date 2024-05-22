ed289402a53bd9130c6a4ba5d5fb50ff0e365886
BONAFO <91232074+BONAFO@users.noreply.github.com>
2024-05-22 09:31:21 -0300

    Add files via upload

---
+++ assets/Home-C6Mm1s0S.js
@@ -0,0 +1,1 @@
+import{u as n}from"./index-B5Mu83hf.js";function r({lang:t}){return{title:"GAME 01"}}const s={__name:"Home",setup(t){const e=n().params;return r({lang:e.lang}),(a,u)=>null}};export{s as default};
