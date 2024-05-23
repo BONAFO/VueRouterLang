15038af4e1a47204f8cc1ab1f7b098a3dbe972c8
BONAFO <91232074+BONAFO@users.noreply.github.com>
2024-05-23 10:18:50 -0300

    Add files via upload

---
+++ assets/Token-D2JD5Rxs.js
@@ -0,0 +1,1 @@
+import{u as a,o,a as s,t as l,e as r}from"./index-BzRjrr4e.js";const c="Loading...",u={title:c};function i({lang:t}){return{title:u.title}}const f={__name:"Token",setup(t){const e=a().params,n=i({lang:e.lang});return(p,_)=>(o(),s("h3",null,l(r(n).title),1))}};export{f as default};
