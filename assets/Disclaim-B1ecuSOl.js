deb7622990ba5071942acbaaecbdc2badcfa435b
BONAFO <91232074+BONAFO@users.noreply.github.com>
2024-05-23 10:11:39 -0300

    Add files via upload

---
+++ assets/Disclaim-B1ecuSOl.js
@@ -0,0 +1,1 @@
+const t="ERES UNA PERSONA?",o="SI, LO SOY",n="NO, NO LOS SOY",s={text:t,yes_button:o,no_button:n};export{s as default,n as no_button,t as text,o as yes_button};
---
+++ assets/Disclaim-D9VBmfW-.js
@@ -0,0 +1,1 @@
+const t="YOU ARE A PERSON?",o="YES, I AM",n="NO, I AM NOT",s={text:t,yes_button:o,no_button:n};export{s as default,n as no_button,t as text,o as yes_button};
