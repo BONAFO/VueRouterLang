d9c1ced9092d794bb74715cfafa46248de887cd4
BONAFO <91232074+BONAFO@users.noreply.github.com>
2024-05-24 13:58:44 -0300

    Add files via upload

---
+++ assets/Token-BtO5sQ2Z.js
@@ -0,0 +1,1 @@
+import{u as a,o,a as s,t as l,d as r}from"./index-vn6MH7sa.js";const c="Loading...",u={title:c};function i({lang:t}){return{title:u.title}}const f={__name:"Token",setup(t){const e=a().params,n=i({lang:e.lang});return(p,_)=>(o(),s("h3",null,l(r(n).title),1))}};export{f as default};
